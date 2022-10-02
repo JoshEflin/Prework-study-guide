42d7b521086650924f10cffc388c6746f2287ea3
ragingpenguin <eflinjh@gmail.com>
2022-10-02 14:25:56 -0400

    javascript added to study guide

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,17 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var topic=0; topic<topics.length; topic++) {
+    console.log(topics[topic])
+}
+/*
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+*/
